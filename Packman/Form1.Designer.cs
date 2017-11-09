d8990be042a01d29d36ede5c532bd8d5a5c5d098
Jacek Czapla <jczapla@asit.pl>
2017-11-09 11:14:58 +0100

    init

--- Packman/Form1.Designer.cs
+++ Packman/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1099, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.openFileToolStripMenuItem.Text = "Open file";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1099, 642);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1099, 666);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PacForm";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Packman/Form1.cs
+++ Packman/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,120 @@ namespace Packman
 {
     public partial class Form1 : Form
     {
+
+        int PacPosX = 9;
+        int PacPosY = 6;
+
+        private int[,] WallsData;
+
+        private int[,] SpotsData;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            if (f.ShowDialog() == DialogResult.Cancel) {
+                return;
+            }
+
+            var lines = File.ReadAllLines(f.FileName);
+            GenerateWallsData(lines);
+
+
+
+        }
+
+        private void GenerateWallsData(string[] lines)
+        {
+            WallsData = new int[lines[0].Length, lines.Length];
+            SpotsData = new int[lines[0].Length, lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int y = 0; y < lines[i].Length; y++)
+                {
+                    var s = lines[i][y];
+                    
+                    WallsData[y, i] = s == '#' ? 0 : 1;
+                    SpotsData[y, i] = s == '#' ? 0 : 1;
+
+                }
+            }
+
+            SpotsData[PacPosX, PacPosY] = 0;
+
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Panel p = sender as Panel;
+            if (p == null)
+                return;
+
+            if (WallsData == null || WallsData.Length <= 0)
+                return;
+
+            //int xsize = WallsData.
+            int xs = WallsData.GetLength(0);
+            int ys = WallsData.GetLength(1);
+
+
+            // labirynt
+            for (int x = 0; x < xs; x++) {
+                for (int y = 0; y < ys; y++)
+                {
+                    Pen blackPen = new Pen(Color.Black, 1);
+                    Brush b = WallsData[x,y] == 0 ? Brushes.Black : Brushes.White;
+
+                    // labirynt
+                    e.Graphics.FillRectangle(b, x * 20 + 20, y * 20 + 20, 20, 20);
+                    // kropka
+                    if (SpotsData[x, y] == 1) {
+                        Brush spotBrush = Brushes.Blue;
+                        e.Graphics.FillRectangle(spotBrush, x * 20 + 27, y * 20 + 27, 6, 6);
+                    }
+                    
+                }
+            }
+
+            // kropki
+
+
+
+            // rysowanie pacmana
+            Brush bred = Brushes.Red;
+            e.Graphics.FillRectangle(bred, PacPosX * 20 + 20 +3, PacPosY * 20 + 20 + 3, 14, 14);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode) {
+                case Keys.Left:
+                    PacPosX = PacPosX - WallsData[PacPosX - 1, PacPosY];
+                    break;
+                case Keys.Right:
+                    PacPosX = PacPosX + WallsData[PacPosX + 1, PacPosY];
+                    break;
+                case Keys.Up:
+                    int s1 = WallsData[PacPosX, PacPosY - 1];
+                    PacPosY = PacPosY - s1;
+                    break;
+                case Keys.Down:
+                    int s2 = WallsData[PacPosX, PacPosY + 1];
+                    PacPosY = PacPosY + s2;
+
+                    break;
+            }
+            SpotsData[PacPosX, PacPosY] = 0;
+
+            panel1.Refresh();
+
         }
     }
 }
